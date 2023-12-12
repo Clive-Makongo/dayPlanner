ccd9848fdbc2fa90932effaded9ea46cd5a362c9
Clive Makongo <clivemakongo@gmail.com>
2023-12-12 17:18:04 +0000

    added save button with ID

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -5,9 +5,6 @@ $(document).ready(function () {
     console.log(today);
     $("#currentDay").text(today.format("D MMM YYYY"));
 
-    // Create input element
-    let input = $("<input>");
-
 
     // Label HTML Elements
     let timeCard = $(".list-group").children("div").children("li");
@@ -15,12 +12,19 @@ $(document).ready(function () {
     console.log(timeCard.length);
 
     for (let i = 0; i < timeCard.length; i++) {
+        let input = $("<textarea>");
+
         timeCard
           .eq(i)
-          .addClass("time" + (9 + i))
-          .attr("data-time", 9 + i)
-          .text(9 + i + " O'clock");
-        console.log(timeCard.eq(i));
+          .attr("id", "time" + (9 + i))
+          .attr("data-time", (9 + i))
+          .text((9 + i) + " O'clock");
+        console.log(timeCard.eq(i),saveButton);
+        saveButton
+          .eq(i)
+          .text("Save")
+          .attr("data-save", (9 + i))
+          .attr("id", "save" + (9 + i));
         input.addClass("input" + (9 + i));
         timeCard.eq(i).append(input);
     }
